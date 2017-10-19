a58983d45ea21db5837bebee3fdf199b066dbdbf
ngootooru <ngootooru@192.168.0.103>
2017-10-19 14:13:13 +0530

    updated code

--- src/main/java/com/java2novice/springboot/SpringBootWebApplication.java
+++ src/main/java/com/java2novice/springboot/SpringBootWebApplication.java
@@ -1,4 +1,4 @@
-package com.java2novice.springboot.starter;
+package com.java2novice.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
