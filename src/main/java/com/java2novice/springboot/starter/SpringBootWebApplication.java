6cea79e82f7054dc6aa15b12696e65d784bd09cd
Gootooru <ngootooru@Nattu-2.local>
2017-10-19 10:54:03 +0530

    initial draft

---
+++ src/main/java/com/java2novice/springboot/starter/SpringBootWebApplication.java
@@ -0,0 +1,12 @@
+package com.java2novice.springboot.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebApplication {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(SpringBootWebApplication.class, args);
+	}
+}
